2336188327f1a879ddb6386a5370517997388a27
wallace-y <cameronwfinney@gmail.com>
2023-09-10 19:27:38 +0100

    adding print results function

--- main.py
+++ main.py
@@ -30,6 +30,15 @@ def get_slot_machine_spin(rows, cols, symbols):
     return columns
 
 
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
+
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -90,7 +99,8 @@ def main():
     print(
         f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}"
     )
-    print(balance, lines)
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
 
 
 main()
